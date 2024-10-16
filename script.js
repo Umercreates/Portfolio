0ce6fae5d17b8a0f4f63ed12932525b59e4157ea
Umer <umershahzadcreates@gmail.com>
2024-10-16 19:14:29 +0500

    Skills

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ let changeText =()=>{
 };
 changeText();
 setInterval(changeText,3000);
+
+// circle Skill Right
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots * marked / 100);  // Corrected Math.floor()
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+    
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked');
+    }
+});
